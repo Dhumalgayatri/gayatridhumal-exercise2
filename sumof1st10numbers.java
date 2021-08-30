6d55b0a27710582e124ec0c6db9bc20830c1d271
Dhumalgayatri <89679091+Dhumalgayatri@users.noreply.github.com>
2021-08-30 22:21:06 +0530

    Add files via upload

---
+++ sumof1st10numbers.java
@@ -0,0 +1,15 @@
+public class Sum_Of_First_10_Natural_Numbers {
+	public static void main(String[] args)   {
+		int i, num = 10, sum = 0;
+		for (i = 1; i <= num; ++i)  {
+			sum = sum + i;
+		}
+		System.out.println("Sum of First 10 Natural Numbers is = " + sum);
+	}
+}
+
+//OUTPUT
+//Sum of First 10 Natural Numbers is = 55
+
+//[Program finished]
+	
